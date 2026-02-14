2d4cdbe1269d1475dd2c4de22b96fbe8a688a3dc
Jan Szotkowski <szotkowski.honz@seznam.cz>
2026-02-14 20:30:38 +0100

    Enhance SEO configuration with default meta tags and updated site details

--- src/utils/seo.ts
+++ src/utils/seo.ts
@@ -1,6 +1,50 @@
 export const siteConfig = {
     title: 'NPM View',
-    description: '',
-    url: 'localhost:3000/',
+    description: 'A fast and comprehensive npm package viewer',
+    url: 'http://localhost:3000',
     author: 'Jan Szotkowski',
+    twitter: '@janszotkowskix',
 };
+
+export const defaultMeta = [
+    {
+        name: 'description',
+        content: siteConfig.description,
+    },
+    {
+        name: 'author',
+        content: siteConfig.author,
+    },
+    {
+        property: 'og:type',
+        content: 'website',
+    },
+    {
+        property: 'og:site_name',
+        content: siteConfig.title,
+    },
+    {
+        property: 'og:title',
+        content: siteConfig.title,
+    },
+    {
+        property: 'og:description',
+        content: siteConfig.description,
+    },
+    {
+        name: 'twitter:card',
+        content: 'summary_large_image',
+    },
+    {
+        name: 'twitter:site',
+        content: siteConfig.twitter,
+    },
+    {
+        name: 'twitter:title',
+        content: siteConfig.title,
+    },
+    {
+        name: 'twitter:description',
+        content: siteConfig.description,
+    },
+];
