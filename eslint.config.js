6d4252ddddb23b6ae44bcc8847653c6ae3bfef2f
Jan Szotkowski <szotkowski.honz@seznam.cz>
2026-02-14 14:27:26 +0100

    Set up ESLint configuration with TypeScript, React, and custom rules integration.

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import react from 'eslint-plugin-react';
+import tseslint from 'typescript-eslint';
+import interfaceToType from 'eslint-plugin-interface-to-type';
+
+export default tseslint.config(
+    {ignores: ['dist', '**/*.stories.*', 'storybook-static', '**/*.test.*']},
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            react,
+            'interface-to-type': interfaceToType,
+        },
+        settings: {
+            react: {
+                version: '19.0',
+            },
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            ...react.configs.recommended.rules,
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+            'react-hooks/refs': 'off',
+            'quotes': ['error', 'single', {avoidEscape: true}],
+            'jsx-quotes': ['error', 'prefer-single'],
+            'react/jsx-max-props-per-line': ['error', {maximum: {single: 1, multi: 1}}],
+            'react/jsx-first-prop-new-line': ['error', 'multiline-multiprop'],
+            'react/jsx-curly-brace-presence': ['error', {props: 'always', children: 'never'}],
+            'react/jsx-indent-props': ['error', 4],
+            'react/jsx-closing-bracket-location': ['error', 'tag-aligned'],
+            'interface-to-type/prefer-type-over-interface': 'error',
+        },
+    },
+);
