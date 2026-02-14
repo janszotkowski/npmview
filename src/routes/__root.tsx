8180df606caf05261679c7228b96f6f1331e8a8d
Jan Szotkowski <szotkowski.honz@seznam.cz>
2026-02-14 14:22:22 +0100

    Integrate Tailwind CSS with Vite setup and improve build configuration.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,12 @@
 /// <reference types="vite/client" />
 import type { ReactNode } from 'react';
 import { createRootRoute, HeadContent, Outlet, Scripts } from '@tanstack/react-router';
+import appCss from '../styles/app.css?url';
 
 export const Route = createRootRoute({
+    headers: () => ({
+        'Cache-Control': 'public, max-age=3600, s-maxage=3600, stale-while-revalidate=86400',
+    }),
     head: () => ({
         meta: [
             {
@@ -12,9 +16,28 @@ export const Route = createRootRoute({
                 name: 'viewport',
                 content: 'width=device-width, initial-scale=1',
             },
+        ],
+        links: [
+            // {
+            //     rel: 'preconnect',
+            //     href: '',
+            // },
+            // {
+            //     rel: 'dns-prefetch',
+            //     href: '',
+            // },
             {
-                title: 'NPM View',
+                rel: 'stylesheet',
+                href: appCss,
             },
+            // {
+            //     rel: 'canonical',
+            //     href: siteConfig.url,
+            // },
+            // {rel: 'icon', href: '/favicon.ico'},
+            // {rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png'},
+            // {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png'},
+            // {rel: 'manifest', href: '/site.webmanifest'},
         ],
     }),
     component: RootComponent,
@@ -30,7 +53,7 @@ function RootComponent() {
 
 function RootDocument({children}: Readonly<{ children: ReactNode }>) {
     return (
-        <html>
+        <html lang={'en'}>
         <head>
             <HeadContent/>
         </head>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,31 @@
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    build: {
+        sourcemap: true,
+        target: 'es2022',
+        minify: 'terser',
+    },
     server: {
         port: 3000,
     },
     plugins: [
         tsConfigPaths(),
-        tanstackStart(),
+        tanstackStart({
+            prerender: {
+                enabled: true,
+                crawlLinks: true,
+            },
+            sitemap: {
+                enabled: true,
+                host: '',
+            },
+        }),
         viteReact(),
+        tailwindcss(),
     ],
 });
